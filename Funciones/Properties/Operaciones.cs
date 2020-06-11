9e1b75e9b961b1101194893d18d3d91b045a2eb6
Clay Arizala <clay.arizala@pucese.edu.ec>
2020-06-11 01:47:40 -0500

    Agregar archivos de proyecto.

---
+++ Funciones/Properties/Operaciones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Properties
+{
+   public class Operaciones
+    {
+        ///<sumary>
+        ///Calcular la suma de uno de los elemntos de un array
+        ///</sumary>sumary>
+        ///<param name=""array"></param>
+        ///<returns>double</returns>
+
+        public double suma(double[] array)
+        {
+            double suma = 0;
+            foreach (double x in array)
+            {
+                suma += x;
+            }
+            return suma;
+        }
+
+        public string fnSaludo(string nombrePersona)
+        {
+            return "Binvenido" + nombrePersona;
+
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ WintestOperaciones/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WintestOperaciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            Funciones.Operaciones op = new Funciones.Operaciones();
+            this.txtnombre.Text = op.saludo("Saul");
+            double[] x = new double[] { 10, 12, 12, 13, 15, 17 };
+            this.txtresultado.Text = op.suma(x).ToString();
+        }
+
+    }
+}
+
+
+
